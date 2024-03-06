b903c3ee9dff6641238b0ec4add53a124b0a81e7
MaheshBhagat101 <149859529+MaheshBhagat101@users.noreply.github.com>
2024-03-06 05:32:36 +0000

    basic operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,16 @@
+let value = 3
+let negValue=-value
+console.log(negValue);
+
+// console.log(2**3); //it means 2 raise to the power 3.
+
+let str1="hello"
+let str2=" Mahesh"
+
+// console.log(str1+str2);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
